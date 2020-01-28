789ea6ee71fce6e03b85f20fd00bb31496eb8bc3
ZivGadri <59201484+ZivGadri@users.noreply.github.com>
2020-01-28 09:44:11 +0200

    WebFlows fixes

--- src/main/java/WorkFlows/WebFlows.java
+++ src/main/java/WorkFlows/WebFlows.java
@@ -1,88 +1,72 @@
 package WorkFlows;  //Represents regular routine tasks and operations that the system does: Creating / removing users,
                     // new tasks etc. - Inherits from CommonOps
 
+import Extensions.UiActions;
 import Utilities.CommonOps;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import javax.swing.*;
 
 public class WebFlows extends CommonOps {
     public static String _userName;
     public static String _projectName;
 
 
-
+    @Step("Sign in to Basecamp")
     public static void signIn(String userEmail, String userPassword){
-        wait.until(ExpectedConditions.visibilityOf(basecampIntroPage.signIn_btn));
-        basecampIntroPage.signIn_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignInPage.userIdentifierInput_Field));
-        basecampSignInPage.userIdentifierInput_Field.sendKeys(userEmail);
-        basecampSignInPage.next_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignInPage.userPasswordInput_Field));
-        basecampSignInPage.userPasswordInput_Field.sendKeys(userPassword);
-        basecampSignInPage.login_btn.click();
+        UiActions.click(basecampIntroPage.signIn_btn);
+        UiActions.insertKeys(basecampSignInPage.userIdentifierInput_Field, userEmail);
+        UiActions.click(basecampSignInPage.next_btn);
+        UiActions.insertKeys(basecampSignInPage.userPasswordInput_Field, userPassword);
+        UiActions.click(basecampSignInPage.login_btn);
     }
 
+    @Step("Sign up to Basecamp")
     public static void signUp(String name, String email, String password, String companyName) {
-        wait.until(ExpectedConditions.visibilityOf(basecampIntroPage.tryItFree_btn));
-        basecampIntroPage.tryItFree_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.startFreeTrial_btn));
-        basecampSignUpFlow.startFreeTrial_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.nextButtonFirst_btn));
-        basecampSignUpFlow.name_Field.clear();
-        basecampSignUpFlow.name_Field.sendKeys(name);
+        UiActions.click(basecampIntroPage.tryItFree_btn);
+        UiActions.click(basecampSignUpFlow.startFreeTrial_btn);
+        UiActions.insertKeys(basecampSignUpFlow.name_Field, name);
         _userName = name;
-        basecampSignUpFlow.email_Field.clear();
-        basecampSignUpFlow.email_Field.sendKeys(email);
-        basecampSignUpFlow.nextButtonFirst_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.createMyAccount_btn));
-        basecampSignUpFlow.password_Field.clear();
-        basecampSignUpFlow.password_Field.sendKeys(password);
-        basecampSignUpFlow.createMyAccount_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.nextButtonSecond_btn));
-        basecampSignUpFlow.companyName_Field.clear();
-        basecampSignUpFlow.companyName_Field.sendKeys(companyName);
-        basecampSignUpFlow.nextButtonSecond_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.nextButtonThird_btn));
-        basecampSignUpFlow.nextButtonThird_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.nextButtonForth_btn));
-        basecampSignUpFlow.nextButtonForth_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampSignUpFlow.allDone_btn));
-        basecampSignUpFlow.allDone_btn.click();
+        UiActions.insertKeys(basecampSignUpFlow.email_Field, email);
+        UiActions.insertKeys(basecampSignUpFlow.password_Field, password);
+        UiActions.click(basecampSignUpFlow.createMyAccount_btn);
+        UiActions.insertKeys(basecampSignUpFlow.companyName_Field, companyName);
+        UiActions.click(basecampSignUpFlow.nextButtonSecond_btn);
+        UiActions.click(basecampSignUpFlow.nextButtonThird_btn);
+        UiActions.click(basecampSignUpFlow.nextButtonForth_btn);
+        UiActions.click(basecampSignUpFlow.allDone_btn);
     }
 
+    @Step("Add new project")
     public static void addNewProject(String projectName, String description) throws InterruptedException {
-        //signIn(getDataFromXML("UserEmail"), getDataFromXML("Password"));
-        wait.until(ExpectedConditions.visibilityOf(basecampMainPage.addAnotherProject_btn));
-        basecampMainPage.addAnotherProject_btn.click();
-        wait.until(ExpectedConditions.visibilityOf(basecampNewProjectFlow.createThisProject_btn));
-        basecampNewProjectFlow.nameThisProject_field.clear();
-        basecampNewProjectFlow.nameThisProject_field.sendKeys(projectName);
+        UiActions.click(basecampMainPage.addAnotherProject_btn);
+        UiActions.insertKeys(basecampNewProjectFlow.nameThisProject_field, projectName);
         _projectName = projectName;
-        basecampNewProjectFlow.addDescription_box.clear();
-        basecampNewProjectFlow.addDescription_box.sendKeys(description);
-        basecampNewProjectFlow.createThisProject_btn.click();
+        UiActions.insertKeys(basecampNewProjectFlow.addDescription_box, description);
+        UiActions.click(basecampNewProjectFlow.createThisProject_btn);
         Thread.sleep(2000);
-        basecampUpperMenu.home_btn.click();
+        UiActions.click(basecampUpperMenu.home_btn);
         Thread.sleep(2000);
     }
 
+    @Step("Remove first project")
     public static void removeAProject(){
         wait.until(ExpectedConditions.visibilityOf(basecampMainPage.addAnotherProject_btn));
         if (basecampMainPage.projects_list.size()>0){
-            basecampMainPage.projects_list.get(0).click();
-            wait.until(ExpectedConditions.visibilityOf(basecampProjectMainPage.optionsMenu_btn));
-            basecampProjectMainPage.optionsMenu_btn.click();
-            basecampRemoveAProjectFlow.menuDeleteThisProject_btn.click();
-            wait.until(ExpectedConditions.visibilityOf(basecampRemoveAProjectFlow.redDeleteThisProject_btn));
-            basecampRemoveAProjectFlow.redDeleteThisProject_btn.click();
+            UiActions.click(basecampMainPage.projects_list.get(0));
+            UiActions.click(basecampProjectMainPage.optionsMenu_btn);
+            UiActions.click(basecampRemoveAProjectFlow.menuDeleteThisProject_btn);
+            UiActions.click(basecampRemoveAProjectFlow.redDeleteThisProject_btn);
             wait.until(ExpectedConditions.visibilityOf(basecampMainPage.addAnotherProject_btn));
         }
     }
 
+    @Step("Change profile image")
     public static void changeProfileImage(String imagePath){
-        wait.until(ExpectedConditions.visibilityOf(basecampMainPage.avatarIcon_btn));
-        basecampMainPage.avatarIcon_btn.click();
-        basecampAvatarMenu.myProfile_btn.click();
-        basecampEditProfilePage.uploadAPhoto_btn.click();
-        basecampEditProfilePage.sendImageElement.sendKeys(imagePath);
+        UiActions.click(basecampMainPage.avatarIcon_btn);
+        UiActions.click(basecampAvatarMenu.myProfile_btn);
+        UiActions.click(basecampEditProfilePage.uploadAPhoto_btn);
+        UiActions.insertKeys(basecampEditProfilePage.sendImageElement, imagePath);
     }                   //impossible due to DOM definitions
 }
